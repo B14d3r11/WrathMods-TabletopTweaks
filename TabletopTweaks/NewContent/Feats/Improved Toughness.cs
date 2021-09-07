edef389823512cdd8c88d016a54049fd928d3119
B14d3r11 <b14d3r11@gmail.com>
2021-09-07 17:30:24 -0400

    Add files via upload

---
+++ TabletopTweaks/NewContent/Feats/Improved Toughness.cs
@@ -0,0 +1,36 @@
+using Kingmaker.Blueprints;
+using Kingmaker.Blueprints.Classes;
+using Kingmaker.Blueprints.Classes.Prerequisites;
+using Kingmaker.Blueprints.Classes.Selection;
+using Kingmaker.Blueprints.Items.Weapons;
+using Kingmaker.Designers.Mechanics.Facts;
+using Kingmaker.Designers.Mechanics.Recommendations;
+using Kingmaker.EntitySystem.Stats;
+using Kingmaker.Enums;
+using Abodell.Config;
+using Abodell.Extensions;
+using Abodell.NewComponents;
+using Abodell.Utilities;
+
+namespace Abodell.NewContent.Feats {
+    static class ImpToughness {
+        public static void AddImpToughness() {
+            var Toughness = Resources.GetBlueprint<BlueprintFeature>("d09b20029e9abfe4480b356c92095623");
+            var ImpToughness = Helpers.CreateBlueprint<BlueprintFeature>("ImpToughness", (System.Action<BlueprintFeature>)(bp => {
+                bp.SetName("Improved Toughness");
+                bp.SetDescription("You have exceptional physical stamina.");
+                bp.ReapplyOnLevelUp = true;
+                bp.IsClassFeature = true;
+				bp.Groups = new FeatureGroup[] { FeatureGroup.Feat };
+//Actually does shit
+				bp.AddComponent(new ToughnessLogic())
+				bp.AddComponent(new ToughnessLogic())
+//Feat Prerequisites
+				bp.AddPrerequisiteFeature(Toughness);
+            }));
+            if (ModSettings.AddedContent.Feats.DisableAll || !ModSettings.AddedContent.Feats.Enabled["ImpToughness"]) { return; }
+            FeatTools.AddAsFeat(ImpToughness);
+        }
+        }
+    }
+}
